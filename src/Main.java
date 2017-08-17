4ac83e280bab5712002ffb72f43a0b78b2b7dfdc
Jose <thompsonjose3@gmail.com>
2017-08-16 19:48:09 -0700

    text file commit

--- src/Main.java
+++ src/Main.java
@@ -20,50 +20,51 @@ public class Main {
         String[] openWords = new String[0];
         String[] prepWords = new String[prepositions.length()];
         String[] dictWords = new String[dictionary.length()];
-        String[] articles = { "a", "the"};
+        String[] articles = {"a", "the"};
 
-        int openLen = openWords.length-1;
-        int prepLen = prepWords.length-1;
-        int dictLen = dictWords.length-1;
+        int openLen = openWords.length - 1;
+        int prepLen = prepWords.length - 1;
+        int dictLen = dictWords.length - 1;
 
 
         // Copying of text file elements to array(s)
 
         // preposition Words
-        prepWords = refArray(prepositions, prepWords);
-        // dictionary Words (british)
-        dictWords = refArray(dictionary, dictWords);
-        // opener Words
-        openWords = refArray(opener, openWords);
+//        prepWords = refArray(prepositions, prepWords);
+//        // dictionary Words (british)
+//        dictWords = refArray(dictionary, dictWords);
+//        // opener Words
+//        openWords = refArray(opener, openWords);
 
 
         // random collection of strings for specificed arrays
 
 
-       // todo** test random integers for fix on selecting random words
+        // todo** test random integers for fix on selecting random words
 
-        String randomPrep = prepWords[((int)(Math.random() * prepLen))];
-        String randomDictWords = dictWords[((int)(Math.random() * dictLen))];
-        String randomOpen = openWords[((int)(Math.random()*openLen))];
-        String randomArticle = articles[((int)(Math.random()*articles.length))];
+        String randomPrep = prepWords[((int) (Math.random() * prepLen))];
+        String randomDictWords = dictWords[((int) (Math.random() * dictLen))];
+        String randomOpen = openWords[((int) (Math.random() * openLen))];
+        String randomArticle = articles[((int) (Math.random() * articles.length))];
 
 
         System.out.println("you look like" + randomArticle + " " + " " + randomDictWords + " " + randomPrep + " " + randomDictWords);
 
     }
+}
 
 
     // todo**  replace reference array and try/catch with URLReader with http, reference strings at beginning.
-
-    public static String[] refArray(String filename, String[] arr){
-        // try-catch method for reading file, placing it in given array, and/or printing error message
-            try {
-                URLreader file = new URLreader(filename);
-                arr = file.OpenFile();
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            }
-        //returns String array
-            return arr;
-    }
-}
+//
+//    public static String[] refArray(String filename, String[] arr){
+//        // try-catch method for reading file, placing it in given array, and/or printing error message
+//            try {
+//                URLreader file = new URLreader(filename);
+//                arr = file.OpenFile();
+//            } catch (IOException e) {
+//                System.out.println(e.getMessage());
+//            }
+//        //returns String array
+//            return arr;
+//    }
+//}
